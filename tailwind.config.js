3b4c2098bd62b7fa4b8f1b323e610bdeadae6ecd
Danilo Silva Rodrigues <danilo.roddrigues@gmail.com>
2021-03-20 16:10:45 -0300

    font family

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        primary: "#FF6363",
+        secodary: {
+          100: "#E2E2E2",
+          200: "#888888"
+        }
+      },
+      fontFamily: {
+        body: ['Nunito']
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
